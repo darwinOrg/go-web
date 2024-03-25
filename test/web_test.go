c9d26dff2516c13691da255bbd5f5a9d069a53b4
chenbiao <149027310@qq.com>
2024-03-25 13:10:36 +0800

    up

--- test/web_test.go
+++ test/web_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"fmt"
 	dgctx "github.com/darwinOrg/go-common/context"
-	"github.com/darwinOrg/go-common/page"
 	"github.com/darwinOrg/go-common/result"
 	"github.com/darwinOrg/go-monitor"
 	"github.com/darwinOrg/go-web/wrapper"
@@ -41,38 +40,6 @@ func TestPost(t *testing.T) {
 		},
 	})
 	_ = engine.Run(fmt.Sprintf(":%d", 8080))
-}
-
-func TestExport(t *testing.T) {
-	engine := wrapper.DefaultEngine()
-
-	wrapper.Get(&wrapper.RequestHolder[wrapper.MapRequest, *result.Result[*result.Void]]{
-		Remark:       "测试get接口",
-		RouterGroup:  engine.Group("/test"),
-		RelativePath: "/get",
-		NonLogin:     true,
-		BizHandler: func(_ *gin.Context, ctx *dgctx.DgContext, request *wrapper.MapRequest) *result.Result[*result.Void] {
-			return result.SimpleSuccess()
-		},
-	})
-
-	wrapper.Post(&wrapper.RequestHolder[UserRequest, *result.Result[*page.PageList[*UserRequest]]]{
-		Remark:       "测试post接口",
-		RouterGroup:  engine.Group("/test"),
-		RelativePath: "post",
-		NonLogin:     true,
-		BizHandler: func(gc *gin.Context, ctx *dgctx.DgContext, request *UserRequest) *result.Result[*page.PageList[*UserRequest]] {
-			return result.Success[*page.PageList[*UserRequest]](nil)
-		},
-	})
-
-	wrapper.ExportSwaggerFile(&wrapper.ExportSwaggerRequest{
-		ServiceName: "test-service",
-		//Title:       "测试服务标题",
-		//Description: "测试服务描述",
-		//OutDir:      "openapi/v1",
-		//Version:     "v0.0.1",
-	})
 }
 
 type UserRequest struct {
