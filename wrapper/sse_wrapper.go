cbbaf6806ab63e572794076b91aaabeac611b373
chenbiao <149027310@qq.com>
2025-05-20 15:31:18 +0800

    sse

--- wrapper/sse_wrapper.go
+++ wrapper/sse_wrapper.go
@@ -1,23 +1,28 @@
 package wrapper
 
 import (
+	"fmt"
+	dgctx "github.com/darwinOrg/go-common/context"
+	dglogger "github.com/darwinOrg/go-logger"
 	"github.com/gin-gonic/gin"
-	"io"
 )
 
-func SseStream(gc *gin.Context, streamFunc func() bool) {
+func SseStream(gc *gin.Context, ctx *dgctx.DgContext, messageChan chan string) {
 	gc.Header("Content-Type", "text/event-stream;charset=utf-8")
 	gc.Header("Cache-Control", "no-cache")
 	gc.Header("Connection", "keep-alive")
-	gc.Stream(func(w io.Writer) bool {
-		return streamFunc()
-	})
+
+	for msg := range messageChan {
+		_, we := gc.Writer.WriteString(msg)
+		if we != nil {
+			dglogger.Errorf(ctx, "writing message error: %v", we)
+		} else {
+			dglogger.Debugf(ctx, "writing message: %s", msg)
+		}
+		gc.Writer.Flush()
+	}
 }
 
-func SseEvent(gc *gin.Context, message any) {
-	gc.SSEvent("data", message)
-}
-
-func SseDone(gc *gin.Context) {
-	gc.SSEvent("data", "DONE")
+func SseMessage(messageChan chan string, message string) {
+	messageChan <- fmt.Sprintf("data: %s\n\n", message)
 }
