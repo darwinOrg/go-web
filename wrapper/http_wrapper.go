c0be5efe3f7f30024361d5bef0091d7e2a54da9b
chenbiao <149027310@qq.com>
2025-05-27 15:45:29 +0800

    up

--- wrapper/http_wrapper.go
+++ wrapper/http_wrapper.go
@@ -1,6 +1,7 @@
 package wrapper
 
 import (
+	dgctx "github.com/darwinOrg/go-common/context"
 	"github.com/darwinOrg/go-common/result"
 	dghttp "github.com/darwinOrg/go-httpclient"
 	dglogger "github.com/darwinOrg/go-logger"
@@ -9,31 +10,30 @@ import (
 	"net/http"
 )
 
-func HttpForward(c *gin.Context, hc *dghttp.DgHttpClient, forwardUrl string) {
-	req, err := http.NewRequest(c.Request.Method, forwardUrl, c.Request.Body)
+func HttpForward(gc *gin.Context, ctx *dgctx.DgContext, hc *dghttp.DgHttpClient, forwardUrl string) {
+	req, err := http.NewRequest(gc.Request.Method, forwardUrl, gc.Request.Body)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusOK, result.SimpleFailByError(err))
+		gc.AbortWithStatusJSON(http.StatusOK, result.SimpleFailByError(err))
 		return
 	}
 
-	req.Header = c.Request.Header
-	ctx := utils.GetDgContext(c)
+	req.Header = gc.Request.Header
 	dglogger.Debugf(ctx, "raw request header: %v", req.Header)
 
 	statusCode, headers, body, err := hc.DoRequest(ctx, req)
 	dglogger.Debugf(ctx, "forward url[%s], statusCode:%d, body:%s", forwardUrl, statusCode, body)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusOK, result.SimpleFailByError(err))
+		gc.AbortWithStatusJSON(http.StatusOK, result.SimpleFailByError(err))
 		return
 	}
 
 	statusCode = utils.AdapterStatusCode(statusCode)
-	c.Status(statusCode)
-	utils.WriteHeaders(c, headers)
+	gc.Status(statusCode)
+	utils.WriteHeaders(gc, headers)
 
 	if len(body) > 0 {
-		_, _ = c.Writer.Write(body)
+		_, _ = gc.Writer.Write(body)
 	} else {
-		_, _ = c.Writer.Write([]byte{})
+		_, _ = gc.Writer.Write([]byte{})
 	}
 }
--- wrapper/sse_wrapper.go
+++ wrapper/sse_wrapper.go
@@ -2,25 +2,17 @@ package wrapper
 
 import (
 	"bufio"
-	"bytes"
 	dgctx "github.com/darwinOrg/go-common/context"
 	"github.com/darwinOrg/go-common/result"
-	dgutils "github.com/darwinOrg/go-common/utils"
 	dghttp "github.com/darwinOrg/go-httpclient"
-	dglogger "github.com/darwinOrg/go-logger"
 	"github.com/darwinOrg/go-web/utils"
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
-	nu "net/url"
-	"strings"
 	"time"
 )
 
-const (
-	jsonContentType     = "application/json; charset=utf-8"
-	sseDefaultSleepTime = time.Millisecond * 10
-)
+const sseDefaultSleepTime = time.Millisecond * 10
 
 type SseBody struct {
 	Event string `json:"event"`
@@ -66,88 +58,52 @@ func SseMessage(messageChan chan *SseBody, event string, message any) {
 	}
 }
 
-func SseForward(gc *gin.Context, hc *dghttp.DgHttpClient, forwardUrl string) {
-	req, err := http.NewRequest(gc.Request.Method, forwardUrl, gc.Request.Body)
+func SseForward(gc *gin.Context, ctx *dgctx.DgContext, hc *dghttp.DgHttpClient, forwardUrl string) {
+	request, err := http.NewRequest(gc.Request.Method, forwardUrl, gc.Request.Body)
+	if err != nil {
+		gc.AbortWithStatusJSON(http.StatusOK, result.SimpleFailByError(err))
+		return
+	}
+	request.Header = gc.Request.Header
+	dghttp.WriteSseHeaders(request)
+
+	resp, err := hc.DoRequestRaw(ctx, request)
 	if err != nil {
 		gc.AbortWithStatusJSON(http.StatusOK, result.SimpleFailByError(err))
 		return
 	}
 
-	req.Header = gc.Request.Header
-	SseRequestRaw(gc, hc, req)
+	WriteSseResponse(gc, resp)
 }
 
 func SseGet(gc *gin.Context, ctx *dgctx.DgContext, hc *dghttp.DgHttpClient, url string, params map[string]string, headers map[string]string) error {
-	if len(params) > 0 {
-		if params != nil && len(params) > 0 {
-			vs := nu.Values{}
-			for k, v := range params {
-				vs.Add(k, v)
-			}
-			url += dgutils.IfReturn(strings.Contains(url, "?"), "&", "?")
-			url += vs.Encode()
-		}
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	resp, err := hc.SseGet(ctx, url, params, headers)
 	if err != nil {
-		dglogger.Errorf(ctx, "new sse get request error, url: %s, err: %v", url, err)
 		return err
 	}
 
-	if len(headers) > 0 {
-		for key, val := range headers {
-			req.Header.Set(key, val)
-		}
-	}
-
-	SseRequestRaw(gc, hc, req)
+	WriteSseResponse(gc, resp)
 	return nil
 }
 
 func SsePostJson(gc *gin.Context, ctx *dgctx.DgContext, hc *dghttp.DgHttpClient, url string, params any, headers map[string]string) error {
-	paramsBytes, err := dglogger.Json(params)
+	resp, err := hc.SsePostJson(ctx, url, params, headers)
 	if err != nil {
-		dglogger.Errorf(ctx, "json marshal error, url: %s, params: %v, err: %v", url, params, err)
-		return err
-	}
-	dglogger.Infof(ctx, "sse post request, url: %s, params: %v", url, string(paramsBytes))
-
-	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(paramsBytes))
-	if err != nil {
-		dglogger.Errorf(ctx, "new sse request error, url: %s, params: %v, err: %v", url, params, err)
 		return err
 	}
 
-	if len(headers) > 0 {
-		for key, val := range headers {
-			req.Header.Set(key, val)
-		}
-	}
-	req.Header.Set("Content-Type", jsonContentType)
-
-	SseRequestRaw(gc, hc, req)
+	WriteSseResponse(gc, resp)
 	return nil
 }
 
-func SseRequestRaw(gc *gin.Context, hc *dghttp.DgHttpClient, req *http.Request) {
-	req.Header.Set("Accept", "text/event-stream")
-	req.Header.Set("Cache-Control", "no-cache")
-	req.Header.Set("Connection", "keep-alive")
-	ctx := utils.GetDgContext(gc)
-
-	resp, err := hc.DoRequestRaw(ctx, req)
-	if err != nil {
-		gc.AbortWithStatusJSON(http.StatusOK, result.SimpleFailByError(err))
-		return
-	}
+func WriteSseResponse(gc *gin.Context, resp *http.Response) {
 	defer func() { _ = resp.Body.Close() }()
 
 	statusCode := utils.AdapterStatusCode(resp.StatusCode)
 	gc.Status(statusCode)
 	utils.WriteHeaders(gc, resp.Header)
-
 	reader := bufio.NewReader(resp.Body)
+
 	for {
 		rawLine, readErr := reader.ReadBytes('\n')
 		if readErr == io.EOF {
